21055f0164df56c0535b2271b8570016f5ab7996
wrunion <winterrunion@gmail.com>
2021-05-31 12:52:08 -0700

    add confirmation to external links.

--- client/scripts.js
+++ client/scripts.js
@@ -1,3 +1,9 @@
+function confirmExternalClick() {
+  return confirm(`This will take you to an external site. Click "OK" to continue or "Cancel" to stay.`);
+}
+  
+  
+  
   // sidebar settings 
   $('.ui.sidebar')
     .sidebar('setting', 'dimPage', false)
@@ -13,6 +19,29 @@
       $('.ui.vertical').removeClass('visible').addClass('right').removeClass('top').sidebar('show');
     }
   });
+function toggle() { $('.ui.sidebar').sidebar('toggle') }
+
+/* custom functionality for the menu */
+navClick = id => {
+  /* grab all "items" in the navbar */
+  let navList = [...document.getElementsByClassName('item')]
+  /* grab the id of the item clicked */
+  let target = document.getElementById(id);
+  /* remove previous "active" classes from all items; add active class to selected item only  */
+  navList.forEach(function(item) {
+    item.classList.remove('active')
+  }); 
+  target.classList.add('active');
+
+  /* scroll to appropriate section */
+  let sectionId = document.getElementById(id).getAttribute("tag");
+  let targetSection = document.getElementById(sectionId)
+  console.log(sectionId, target)
+
+  targetSection.scrollIntoView();
+}
+
+
 
   // for gif "pause feature" in "work" section 
   // accessibility code for users who have the "reduced motion" setting enabled on their browsers
